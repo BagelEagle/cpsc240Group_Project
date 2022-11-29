14444427f0c91178f87885a5d9e77eea4f301e9e
jamador <jamador@mail.umw.edu>
2022-11-29 11:28:08 -0500

    Created a method that initializes the Login Window. Also created a method that displays and hides the login window. No buttons have been added yet.

--- src/Window.java
+++ src/Window.java
@@ -4,16 +4,39 @@ import javax.swing.*;
 
 public class Window {
     private JFrame login;
+    private JFrame newUser;
     private JFrame menu;
     private JFrame deposit;
     private JFrame withdraw;
     private JFrame transfer;
     private JFrame requestLoan;
     private JFrame checkBalance;
-
-    public JFrame getLogin () {
-        return login;
+    public Window(){
+        initializeLoginWindow();
     }
+
+    /**
+     * @author James Amador
+     * Creates a Login window for users to input their
+     * login and password. Note that this method is only
+     * called in the constructor of the Window class.
+     */
+    public void initializeLoginWindow(){
+        login = new JFrame("Login Page");
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.getContentPane().setLayout(new BoxLayout(login.getContentPane(), BoxLayout.Y_AXIS));
+        //function that adds login field.
+        //NOTE: I'm going to move the function from main into this class
+    }
+
+    public void displayLogin() {
+        login.pack();
+        login.setVisible(true);
+    }
+    public void hideLogin(){
+        login.setVisible(false);
+    }
+    public JFrame getNewUser(){ return newUser; }
 
     public JFrame getMenu () {
         return menu;
